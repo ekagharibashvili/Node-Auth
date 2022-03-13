5e7b7bd90a91c88459a060acee2802a4a8cd9115
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-03-13 23:30:04 +0400

    finish project

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -7,6 +7,7 @@ var User = require("../user/User");
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 var config = require("../config");
+var VerifyToken = require("./VerifyToken");
 
 router.post("/register", function (req, res) {
   var hashedPassword = bcrypt.hashSync(req.body.password, 8);
@@ -28,24 +29,39 @@ router.post("/register", function (req, res) {
   );
 });
 
-router.get("/me", function (req, res) {
-  var token = req.headers["x-access-token"];
-  if (!token)
-    // 401 unauthorized status
-    return res.status(401).send({ auth: false, message: "No token provided." });
-  jwt.verify(token, config.secret, function (err, decoded) {
+router.get("/me", VerifyToken, function (req, res, next) {
+  User.findById(decoded.id, { password: 0 }, function (err, user) {
     if (err)
-      return res
-        .status(500)
-        .send({ auth: false, message: "Failed to authenticate token." });
-    User.findById(decoded.id, { password: 0 }, function (err, user) {
-      if (err)
-        return res.status(500).send("There was a problem finding the user.");
-      if (!user) return res.status(404).send("No user found.");
+      return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
 
-      res.status(200).send(user);
-    });
+    res.status(200).send(user);
+    //  next(user);
   });
+});
+
+//midleware
+/* router.use(function (user, req, res, next) {
+   res.status(200).send(user);
+  }); */
+
+router.post("/login", function (req, res) {
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) return res.status(500).send("Eroor on the server.");
+    if (!user) return res.status(404).send("No user found");
+    var passwordValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordValid)
+      return res.status(401).send({ auth: false, token: null });
+
+    var token = jwt.sign({ id: user._id }, config.secret, {
+      expiresIn: 86400, // expires in 24 hours
+    });
+    res.status(200).send({ auth: true, token: token });
+  });
+});
+
+router.get("/logout", function (req, res) {
+  res.status(200).send({ auth: false, token: null });
 });
 
 module.exports = router;
