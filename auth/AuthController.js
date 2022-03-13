f437d3757ac3dae7c694d8e1386b6163c8838b1c
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-03-13 18:37:17 +0400

    feat: add routers

---
+++ auth/AuthController.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.router();
+const bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+const User = require("../user/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const config = require("../config");
+
+router.post("/register", function (req, res) {
+  let hashedPassword = bcrypt.hashSync(req.body.password, 8);
+  User.create(
+    {
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    },
+    function (err, user) {
+      if (err)
+        return res.status(500).send("There was a problem registering a user");
+      // create a token for the user
+      let token = jwt.sign({ id: user._id }, config.secret, {
+        expiresIn: 86400, // expires in 24 hours
+      });
+      res.status(200).send({ auth: true, token: token });
+    }
+  );
+});
+
+router.get("/me", function (req, res) {
+  let token = req.headers["x-access-token"];
+  if (!token)
+    // 401 unauthorized status
+    return res.status(401).send({ auth: false, message: "No token provided." });
+  jwt.verify(token, config.secret, function (err, decoded) {
+    if (err)
+      return res
+        .status(500)
+        .send({ auth: false, message: "Failed to authenticate token." });
+    res.status(200).send(decoded);
+  });
+});
+
+module.exports = router;
